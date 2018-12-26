3010e87563d541d25c9c9888b7bd9c326dbda640
Jason Yu <xuzj03@gmail.com>
2018-12-26 23:30:56 +0800

    Refine the script to apply patches
     - Remove the older patches before copy the
       new patches.
    
    Signed-off-by: Jason Yu <xuzj03@gmail.com>

--- apply_rv32m1_patches.sh
+++ apply_rv32m1_patches.sh
@@ -4,18 +4,22 @@ echo "Checking out pulp-riscv-gnu-toolchain"
 git checkout 940ca73fa36e17805f176a5096c19d1cf375d223
 git submodule update -f --init --recursive
 
-# Copy patch
-echo "Copy RM32M1 patch"
-cp -r rv32m1_patches/* ./
-
-# Apply patch
-echo "Apply RM32M1 patch"
-git am *.patch
-
-for folder in $(ls -d rv32m1_patches/*/)
+for folder in $(find rv32m1_patches/ -maxdepth 2 -type d)
 do
-    subfolder=${folder/#rv32m1_patches\/}
+    subfolder="./"${folder/#rv32m1_patches\/}
+
+    if [[ -z $subfolder ]]
+    then
+    echo "ERROR: empty subfolder name"
+    exit
+    fi
+
     echo "Apply RM32M1 patch in" $subfolder
+    # Remove the older patch
+    rm ./$subfolder/*.patch
+    # Copy new patch
+    cp rv32m1_patches/$subfolder/*.patch ./$subfolder/
+    # Apply patch
     cd $subfolder
     git am *.patch
     cd -
